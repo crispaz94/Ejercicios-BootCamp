b7290c26aa81b642a917cb3f6f415ff51b4be5c3
Cristian Paz <cristianpaz17@gmail.com>
2023-02-19 14:01:44 -0600

    Ejercicio 1 y 2

---
+++ Ejercicios Sesion 1/Ejercicios1/Program.cs
@@ -0,0 +1,20 @@
+﻿/**
+ * Ejercicio 1
+ * Escribe un programa que reciba tu nombre y lo escriba por consola.
+ * **/
+
+Console.WriteLine("Ingresa tu nombre:");
+string nombre = Console.ReadLine();
+Console.WriteLine("Hola, " + nombre);
+
+
+/*
+ * Ejercicio 2
+ * Escribe un programa que tome la hora y la escriba por pantalla.
+**/
+
+//Console.WriteLine("Ingresa la hora a mostrar");
+//string hora = Console.ReadLine();
+//Console.WriteLine("Ingresa la jornada am O pm");
+//string jornada = Console.ReadLine();
+//Console.WriteLine("La hora ingresada es: " + hora + " " +jornada);
