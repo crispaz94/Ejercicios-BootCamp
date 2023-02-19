2c0beed8c580271dbfb348f21670cbb0d965ad13
Cristian Paz <cristianpaz17@gmail.com>
2023-02-19 14:22:12 -0600

    Commit Ejercicio 2

---
+++ Ejercicios Sesion 2/EjerciciosSesion2/EjerciciosSesion2/Program.cs
@@ -0,0 +1,80 @@
+﻿/**
+ Ejercicio 1
+
+  Variables Escribe un programa que reciba datos de una persona y genera un mensaje,
+  usa una variable para cada dato y otra para el mensaje. Ej: nombre, apellido, edad, sabe programar, etc.
+
+ */
+
+/*
+Console.WriteLine("Ingresa tu nombre: ");
+string name = Console.ReadLine();
+
+Console.WriteLine("Ingresa tu apellido: ");
+string apellido = Console.ReadLine();
+
+Console.WriteLine("Ingresa tu edad: ");
+int edad = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Sabe Programar? True para si, False para no: ");
+bool sabe = Convert.ToBoolean(Console.ReadLine());
+
+Console.WriteLine("Hola " + name + " " + apellido + " ,tu edad es: " + edad + " y " + sabe + " en programar");
+*/
+
+/*
+ *Ejercicio 2
+
+Tipos Usando los tipos de variables más adecuados, describe los objetos siguientes:
+
+Coche: puertas, ruedas, marca, ITV vigente
+
+Mesa: peso, largo, material, color
+
+Nota: puedes escribir estos datos por consola si prefieres verlos. La idea del ejercicio es almacenar los datos en los tipos más adecuados.
+
+**/
+
+/*
+//Coche
+int puertas = 4;
+int ruedas = 4;
+string marca = "Toyota";
+int ITV = 10;
+
+//Mesa
+float peso = 12.32f;
+float largo = 12.23f;
+float material = 14.56f;
+string color = "Azul";
+*/
+
+/*
+
+Ejercicio 3
+
+Operadores Determina los operadores para verificar las siguientes condiciones:
+
+Un número es mayor o igual a 18
+
+Un char es ‘a’
+
+Se cumplen dos conciones a la vez (ambas verdaderas)
+
+Se cumple una de dos condiciones a la vez (una true y otra false)
+
+Nota: puedes escribir estos datos por consola si prefieres verlos. La idea del ejercicio es almacenar los datos en los tipos más adecuados.
+
+*/
+
+/*
+int a = 10;
+int b = 15;
+char c = 'a';
+int d = 18;
+
+Console.WriteLine(d >= 18);
+Console.WriteLine(c == 'a');
+Console.WriteLine(a<b && b>a);
+Console.WriteLine(a > b && b > a);
+*/
