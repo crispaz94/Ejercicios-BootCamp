a79a5edf68f89fbcc7464366d91d6fe7b1ecbe7c
Cristian Paz <cristianpaz17@gmail.com>
2023-02-19 15:25:36 -0600

    Ejercicio Sesion 3

---
+++ Ejercicios Sesion 3/EjerciciosSesion3/EjerciciosSesion3/Program.cs
@@ -0,0 +1,46 @@
+﻿/**
+ Ejercicio 1
+
+Crea una estructura de datos para un cliente con estos campos:
+
+Nombre completo
+
+Teléfono
+
+Dirección
+
+Email
+
+Si es nuevo cliente
+
+Bonus: escribe un método para presentar estos datos desde la estructura al hacer Console.WriteLine(...)
+ 
+ 
+ */
+
+
+Client myClient = new Client("Cristian", 33259876, "Honduras", "crpaz21@hotmail.com");
+Console.WriteLine(myClient);
+
+
+public struct Client
+{
+
+    public Client(string name, long phone, string address, string email)
+    {
+        Name = name;
+        Phone = phone;
+        Address = address;
+        Email = email;
+    }
+
+    public string Name { get; set; }
+    public long Phone { get; set; }
+    public string Address { get; set; }
+    public string Email { get; set; }
+
+    public override string ToString()
+    {
+        return $"Hello {Name} {Address} {Phone} {Email}";
+    }
+}
